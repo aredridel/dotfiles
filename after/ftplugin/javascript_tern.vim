ternjs/tern_for_vim

added after  directory to override runtime ftplugin and created menus

after/ftplugin/javascript_tern.vim
@@ -1,0 +1,8 @@
+call tern#Enable()
+
+" Menu 
+menu <silent> Tern.Jump\ To\ Defintion :TernDef<CR>
+menu <silent> Tern.See\ Documentation :TernDoc<CR>
+menu <silent> Tern.DataType :TernType <CR>
+menu <silent> Tern.Show\ all\ References :TernRefs<CR>
+menu <silent> Tern.Rename :TernRenam <CR> 

aredridel/dotfiles

after/ftplugin/javascript_tern.vim
@@ -1,0 +1,8 @@
+call tern#Enable()
+
+" Menu 
+menu <silent> Tern.Jump\ To\ Defintion :TernDef<CR>
+menu <silent> Tern.See\ Documentation :TernDoc<CR>
+menu <silent> Tern.DataType :TernType <CR>
+menu <silent> Tern.Show\ all\ References :TernRefs<CR>
+menu <silent> Tern.Rename :TernRename <CR> 