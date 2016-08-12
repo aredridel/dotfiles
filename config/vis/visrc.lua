f038aef184e423bec12a4a8d7e8c06b6c4a5eec1
Aria Stewart <aredridel@dinhe.net>
2016-08-12 10:43:40 -0400

    config for vis

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -4,4 +4,7 @@ require('vis')
 vis.events.win_open = function(win)
 	vis.filetype_detect(win)
 	vis:command('set theme dark-16')
+	vis:command('set tabwidth 2')
+	vis:command('set expandtab yes')
+	vis:command('set autoindent yes')
 end
