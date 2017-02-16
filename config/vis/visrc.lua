a122eaa110847dc96721346c6338b274fe36bcdf
Aria Stewart <aredridel@nbtsc.org>
2017-02-16 11:14:09 -0500

    modern visrc

--- config/vis/visrc.lua
+++ config/vis/visrc.lua
@@ -1,10 +1,17 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+require('plugins/filetype')
+require('plugins/textobject-lexer')
 
-vis.events.win_open = function(win)
-	vis.filetype_detect(win)
-	vis:command('set theme subtle-256')
+vis.events.subscribe(vis.events.INIT, function()
+--	vis:command('set theme subtle-256')
+--	vis:command('set theme light-16')
+	vis:command('set theme solarized')
+end)
+
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command('set tabwidth 2')
 	vis:command('set expandtab yes')
 	vis:command('set autoindent yes')
-end
+end)
