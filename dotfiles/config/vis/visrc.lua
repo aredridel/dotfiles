0969dae772241167a04be0bff8ec05b88c9234fd
Aria Stewart <aredridel@dinhe.net>
2024-01-21 16:21:38 -0500

    stuff

--- dotfiles/config/vis/visrc.lua
+++ dotfiles/config/vis/visrc.lua
@@ -1,18 +1,20 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
-require('plugins/filetype')
-require('plugins/textobject-lexer')
+-- require('plugins/filetype')
+-- require('plugins/textobject-lexer')
 require('plugins/surround')
+lspc = require('plugins/vis-lspc')
+lspc.ls_map.javascript.cmd = 'typescript-language-server --stdio --tsserver-path=/opt/homebrew/bin/tsserver'
 
-vis.events.subscribe(vis.events.INIT, function()
+--vis.events.subscribe(vis.events.INIT, function()
 --	vis:command('set theme subtle-256')
 --	vis:command('set theme light-16')
-	vis:command('set theme importance')
-end)
+--	vis:command('set theme importance')
+-- end)
 
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set tabwidth 2')
-	vis:command('set expandtab yes')
-	vis:command('set autoindent yes')
-end)
+-- vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+--	vis:command('set tabwidth 2')
+--	vis:command('set expandtab yes')
+--	vis:command('set autoindent yes')
+--end)
