63312ad4a086112c225adde6de51d80f065b3779
Aria Stewart <aredridel@dinhe.net>
2025-02-23 16:08:41 -0500

    stuff

--- dotfiles/config/nvim/lua/config/lazy.lua
+++ dotfiles/config/nvim/lua/config/lazy.lua
@@ -22,7 +22,7 @@ require("lazy").setup({
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
+  -- Colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = false },
