b600004a3958ac7dd4152159a455a2832107c26d
Aria Stewart <aredridel@dinhe.net>
2025-04-14 10:04:46 -0400

    luasnip

---
+++ dotfiles/config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,8 @@
+return {
+    "L3MON4D3/LuaSnip",
+    -- Follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+}
+
