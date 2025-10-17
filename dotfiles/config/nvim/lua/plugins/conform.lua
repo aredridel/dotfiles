5f0106cfe0e8503ba9b9a9447018a20613756655
Aria Stewart <astewart@spicelabs.io>
2025-10-17 13:35:59 -0400

    wip

--- dotfiles/config/nvim/lua/plugins/conform.lua
+++ dotfiles/config/nvim/lua/plugins/conform.lua
@@ -1,10 +1,18 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      typescript = { 'prettierd', 'prettier', stop_after_first = true },
-      vue = { 'prettierd', 'prettier', stop_after_first = true },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			vue = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			svelte = { "prettierd", "prettier", stop_after_first = true },
+			lua = { "stylua" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
 }
